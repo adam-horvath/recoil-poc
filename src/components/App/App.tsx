c091be16e51687825a2f1e8f36aee0267f7fdb58
adam-horvath <horvathady@gmail.com>
2024-05-07 13:19:53 +0200

    useRecoilValue example

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,14 @@
 import { TodoList } from 'components/Todo/TodoList';
+import { useRecoilValue } from 'recoil';
+import { todoList } from 'recoilState/todo';
 
 function App() {
+  const list = useRecoilValue(todoList);
+
   return (
     <div className={'App'}>
       <h1 className={'mb-5'}>Recoil Example</h1>
+      <h2 className={'mb-3'}>Current list size is {list.length}</h2>
       <TodoList />
     </div>
   );
--- src/components/Todo/TodoItem.tsx
+++ src/components/Todo/TodoItem.tsx
@@ -13,13 +13,13 @@ export const TodoItem: FC<TodoItemProps> = ({ onDelete, todo }) => {
   const { id, title, due_on, status } = todo;
 
   const deleteTodo = () => {
-    onDelete(id || 0);
+    onDelete(id ?? 0);
   };
 
   return (
     <div className={'row mb-3 justify-content-start align-items-start'} style={{ height: 50 }}>
       <div className={'pe-3 col-4'}>{title}</div>
-      <div className={'pe-3 col-3'}>{formatDate({ date: due_on, handleTime: true })}</div>
+      <div className={'pe-3 col-3'}>{formatDate({ date: new Date(due_on), handleTime: true })}</div>
       <div className={'pe-3 col-2'}>{status}</div>
       <div className={'text-center col-3 d-flex '}>
         <Button onClick={deleteTodo}>Delete</Button>
